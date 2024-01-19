d0b8c30b2da18e1044b34a38323c468c20f92103
anwesha2812 <78586680+anwesha2812@users.noreply.github.com>
2024-01-19 12:24:34 +0530

    Update streamlit_selenium.py

--- streamlit_selenium.py
+++ streamlit_selenium.py
@@ -8,17 +8,42 @@ nltk.download('stopwords')
 from youtube_transcript_api import YouTubeTranscriptApi
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.chrome.service import Service as ChromeService
+
 # Replace 'YOUR_API_KEY' with your actual API key
-api_key = "AIzaSyCk5y59_XIlabK3bDipO7FdPSD7lBguxIA"
+api_key = "AIzaSyDbCm_iBKgepcYtJY2xcQZ-Udf8Qwuytwk"
 
 
 # URL of your Google Apps Script web app
-apps_script_url =  'https://script.google.com/macros/s/AKfycbyH2Dma-c3MLo2QnxKqnSRgbInX6nC48vS23PL4h7HuLoCfDSApaF4LzH6kWnctphY/exec'
+apps_script_url =  'https://script.google.com/macros/s/AKfycbyhJ4zrgkDYfVJNTiBgfzc0KzVAbaVi1pjBJ6CrLQsWkn4WIbxkbVY-4OB84KMScNU7Wg/exec'
+
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
+from langchain.llms import Replicate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import os
+from dotenv import load_dotenv
+import tempfile
+
+st.set_page_config(layout="wide")
+
+load_dotenv()
+
+from frontend import initialize_session_state, conversation_chat, display_chat_history, create_conversational_chain
 
 def get_video_title(api_key, video_id):
     base_url = "https://www.googleapis.com/youtube/v3/videos"
@@ -87,8 +112,6 @@ def generate_pdf(text, output_file):
     
     # Save the PDF
     c.save()
-# transcription
-
 
 def get_course_info(subject):
     # Set up ChromeOptions for headless mode
@@ -112,123 +135,188 @@ def get_course_info(subject):
             {
                 'title': title.find_element(By.CLASS_NAME, "cds-119.cds-CommonCard-title.css-e7lgfl.cds-121").text.strip(),
                 'link': title.find_element(By.CLASS_NAME, "cds-119.cds-113.cds-115.cds-CommonCard-titleLink.css-si869u.cds-142").get_attribute('href'),
-                'image': title.find_element(By.CLASS_NAME, "cds-9.css-0.cds-11.cds-grid-item.cds-56.cds-64.cds-76 img").get_attribute('src')
+                'image': title.find_element(By.CLASS_NAME, "cds-9.css-0.cds-11.cds-grid-item.cds-56.cds-64.cds-76 img").get_attribute('src'),
+                'ratings': title.find_element(By.CLASS_NAME, "cds-CommonCard-ratings").text.strip()
             }
             for title in course_elements[:5]
         ]
 
     return course_info
+# transcription
+
 
 
 # Define the Streamlit app
 def main():
     # Set title of the browser tab
-    st.set_page_config(page_title="Sensei Extension")
+    #st.set_page_config(page_title="Sensei Extension")
 
     # Set the title of the Streamlit app
-    st.title("Sensei Extension")   
+    st.title("Sensei Extension")
+
+    # Create columns for layout
+    col1, col2 = st.columns([3,2])
+
     # Get query parameters
     youtube_url = st.experimental_get_query_params().get("youtube_url", [""])[0]
 
-    if youtube_url:
-        # st.write(f"Embedding YouTube video from URL: {youtube_url}")
+    with col1:
+            
+        with st.expander('**PRE-REQUISITES**'):
+            st.write('The output for pre-requisites here')
+        if youtube_url:
+            # st.write(f"Embedding YouTube video from URL: {youtube_url}")
 
-        # Use st.video to embed the YouTube video
-        st.video(youtube_url)
-         # print(youtube_url)
-        video_id = get_video_id(youtube_url)
+            # Use st.video to embed the YouTube video
+            st.video(youtube_url)
+            # print(youtube_url)
+            video_id = get_video_id(youtube_url)
 
-        output_file = "output.pdf"  # Output PDF file name
+            output_file = "output.pdf"  # Output PDF file name
 
-        # Generate transcription
-        transcription = generate_transcription(video_id)
+            # Generate transcription
+            transcription = generate_transcription(video_id)
 
-        # Generate PDF
-        generate_pdf(transcription, output_file)
+            # Generate PDF
+            generate_pdf(transcription, output_file)
+            
+            # print(video_id)
+            # Get the YouTube video URL from the URL parameters
         
-        # print(video_id)
-        # Get the YouTube video URL from the URL parameters
-    
 
-        user_input = remove_non_alphabetic(str(get_video_title(api_key,video_id)))
-        sentence = user_input
-        user_input = remove_stopwords(sentence)
-        print(user_input)
-        # Check if a YouTube URL is provided
+            user_input = remove_non_alphabetic(str(get_video_title(api_key,video_id)))
+            sentence = user_input
+            user_input = remove_stopwords(sentence)
+            print("=====================")
+            print(user_input)
+            # Check if a YouTube URL is provided
 
+            with st.expander('**RECOMMENDED VIDEOS**'):
+                st.write("Recommended videos on the topic:")
+                search_term =  user_input
+                params = {"q": search_term}
+                response = requests.get(apps_script_url, params=params)
+            
+                if response.status_code == 200:
+                    try:
+                        
+                        data = response.json()
+                        print("=====================")
+                        print(data)
+                        videos = data["videos"]
 
-        st.write("Recommended videos on the topic:")
-        search_term =  user_input
-        params = {"q": search_term}
-        response = requests.get(apps_script_url, params=params)
-    
-        if response.status_code == 200:
-            try:
-                
-                data = response.json()
-                print(data)
-                videos = data["videos"]
+                        # print("=======================")
+                        # print(videos)
+                        # print("=======================")
+                        
+                        top_videos = videos[:5]  # Get the top 5 videos with the highest view counts
 
-                # print("=======================")
-                # print(videos)
-                # print("=======================")
-                
-                top_videos = videos[:5]  # Get the top 5 videos with the highest view counts
+                        # Display the top videos in a row
+                        st.header("Top 5 Videos with the Highest View Counts", anchor='center')  # Anchor the header to the center
 
-                # Display the top videos in a row
-                st.header("Top 5 Videos with the Highest View Counts", anchor='center')  # Anchor the header to the center
+                        for i, video in enumerate(top_videos):
+                            # Create a container for thumbnail, table, and title
+                            st.markdown(
+                                f"<div style='display: flex; align-items: center; margin: 10px;'>"
+                                f"<a href='https://www.youtube.com/watch?v={video['videoId']}' target='_blank'>"
+                                f"<img src='https://img.youtube.com/vi/{video['videoId']}/default.jpg' width='240' height='180'></a>"
+                                f"<div style='margin-left: 10px;'>"
+                                f"<h3 style='font-size: 18px;'>{video['videoTitle']}</h3>"  # Adjust the font size here
+                                f"<table style='text-align: center; margin-top: 10px;'><tr><th>View Count</th><th>Like Count</th></tr><tr>"
+                                f"<td>{int(video['statistics']['viewCount']):,}</td>"
+                                f"<td>{int(video['statistics']['likeCount']):,}</td></tr></table></div></div>",
+                                unsafe_allow_html=True
+                            )
+                    except ValueError as e:
+                        html_content = response.text
+                        soup = BeautifulSoup(html_content, 'html.parser')
+                        pretty_html = soup.prettify()
+                        st.markdown(pretty_html, unsafe_allow_html=True)
+                        st.error(f"Error parsing JSON: {e}")
+                else:
+                    st.error("Error fetching data from the YouTube API, Status code: {response.status_code}")
+                    # Send the input to the Google Apps Script using a GET request
+            
+            with st.expander('**RECOMMENDED COURSES**'):
+                subject_to_search = st.text_input("Enter the subject:", "Enter your subject....")
 
-                for i, video in enumerate(top_videos):
-                    # Create a container for thumbnail, table, and title
-                    st.markdown(
-                        f"<div style='display: flex; align-items: center; margin: 10px;'>"
-                        f"<a href='https://www.youtube.com/watch?v={video['videoId']}' target='_blank'>"
-                        f"<img src='https://img.youtube.com/vi/{video['videoId']}/default.jpg' width='240' height='180'></a>"
-                        f"<div style='margin-left: 10px;'>"
-                        f"<h3 style='font-size: 18px;'>{video['videoTitle']}</h3>"  # Adjust the font size here
-                        f"<table style='text-align: center; margin-top: 10px;'><tr><th>View Count</th><th>Like Count</th></tr><tr>"
-                        f"<td>{int(video['statistics']['viewCount']):,}</td>"
-                        f"<td>{int(video['statistics']['likeCount']):,}</td></tr></table></div></div>",
-                        unsafe_allow_html=True
-                    )
-            except ValueError as e:
-                html_content = response.text
-                soup = BeautifulSoup(html_content, 'html.parser')
-                pretty_html = soup.prettify()
-                st.markdown(pretty_html, unsafe_allow_html=True)
-                st.error(f"Error parsing JSON: {e}")
+                # Display a submit button
+                submit_button = st.button("Submit")
+
+                # Check if the submit button is clicked
+                if submit_button:
+                    # Display courses with images, titles, and ratings in separate columns using Streamlit
+                    st.title(f"Coursera {subject_to_search.capitalize()} Courses")
+
+                    # Check if the user has entered a subject
+                    if subject_to_search:
+                        course_info = get_course_info(subject_to_search)
+
+                        for info in course_info[:5]:
+                            # Display each container with thumbnail in the left column, title in the center column, and ratings in the right column
+                            st.markdown(
+                                f'<div class="row" style="margin: 10px; display: flex; align-items: center; justify-content: space-around;">'
+                                f'   <div style="flex: 1;"><a href="{info["link"]}" target="_blank"><img src="{info["image"]}" alt="{info["title"]}" style="width: 120px; height: 120px;"></a></div>'
+                                f'   <div style="flex: 2; text-align: justify; font-weight: bold; margin: 10px;"><a href="{info["link"]}" target="_blank">{info["title"]}</a></div>'
+                                f'   <div style="flex: 1; text-align: center; margin: 10px;">{info["ratings"]}</div>'
+                                f'</div>', unsafe_allow_html=True
+                            )
+
+                        st.write("---")
+                else:
+                    st.warning("Please enter a subject to search for courses.")
+
         else:
-            st.error("Error fetching data from the YouTube API, Status code: {response.status_code}")
-            # Send the input to the Google Apps Script using a GET request
+            st.warning("No YouTube video URL provided.")
 
-        subject_to_search = st.text_input("Enter the subject:", "Enter your subject...")
+    load_dotenv()
 
-        # Display a submit button
-        submit_button = st.button("Submit")
+    # Initialize Streamlit
+    uploaded_files = st.file_uploader("Upload files", accept_multiple_files=True)
 
-        # Check if the submit button is clicked
-        if submit_button:
-            # Display courses with images and titles in separate columns using Streamlit
-            st.title(f"Coursera {subject_to_search.capitalize()} Courses")
 
-            # Check if the user has entered a subject
-            if subject_to_search:
-                course_info = get_course_info(subject_to_search)
+    with col2:
+        if uploaded_files:
+            text = []
+            for file in uploaded_files:
+                file_extension = os.path.splitext(file.name)[1]
+                with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                    temp_file.write(file.read())
+                    temp_file_path = temp_file.name
 
-                for info in course_info[:5]:
-                    # Display each container with thumbnail in the left column and title in the right column
-                    st.markdown(
-                        f'<div class="row" style="margin: 10px; display: flex; align-items: center; justify-content: space-around;">'
-                        f'   <div style="flex: 1;"><a href="{info["link"]}" target="_blank"><img src="{info["image"]}" alt="{info["title"]}" style="width: 120px; height: 120px;"></a></div>'
-                        f'   <div style="flex: 2; text-align: justify; font-weight: bold; margin: 10px;"><a href="{info["link"]}" target="_blank">{info["title"]}</a></div>'
-                        f'</div>', unsafe_allow_html=True
-                    )
+                loader = None
+                if file_extension == ".pdf":
+                    loader = PyPDFLoader(temp_file_path)
+                elif file_extension == ".docx" or file_extension == ".doc":
+                    loader = Docx2txtLoader(temp_file_path)
+                elif file_extension == ".txt":
+                    loader = TextLoader(temp_file_path)
 
-                st.write("---")
-        else:
-            st.warning("Please enter a subject to search for courses.")
-    else:
-        st.warning("No YouTube video URL provided.")
+                if loader:
+                    text.extend(loader.load())
+                    os.remove(temp_file_path)
+
+            text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=100, length_function=len)
+            text_chunks = text_splitter.split_documents(text)
+
+            # Create embeddings
+            embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
+                                            model_kwargs={'device': 'cpu'})
+
+            # Create vector store
+            vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
+
+            # Initialize session state
+            initialize_session_state()
+
+            # Create the chain object
+            chain = create_conversational_chain(vector_store)
+
+            
+            display_chat_history(chain)
+
+        with st.expander('**QUIZ BASED ON THE TOPIC**'):
+            st.write('Quiz to provide quiz related to the topic')
 
     hide_streamlit_style = """
                 <style>
@@ -237,10 +325,6 @@ def main():
                 </style>
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
